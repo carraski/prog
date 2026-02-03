741d27f2eb299d995dfc80f983d7fae389c2cadd
carraski <carrasquerrur@ieselaios.com>
2026-02-03 08:40:32 +0000

    hecho el bucle con el if

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,30 +1,36 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-int main(){
+int main() {
+    cout << "¿Qué quieres que te diga? (1, 2 o 3)\n";
+    int opcion;
     int i;
-    i=0;
+    cin >> opcion;
+     if(opcion == 1){
+     i=0;
     while(i<6){
-         cout << "Buenos días estrellitas\n";
-         i=i+1;
+        cout << "Buenos días estrellita\n";
+i=i+1;
     }
-   std::cout << "¿que quieres que te diga? ¿1, 2 o 3?"
-   std::cin >> 1, 2 o 3;
-    int v;
+    }
+    
+    else if(opcion == 2){
+      int v;
     v=0;
-    while(v<1299){
-         cout << "Eres super guapo, inteligente y buena persona, Raúl\n";
-         v=v+1;
+     while(v<1299){
+        cout << "Eres super guapo, inteligente y buena persona, Raúl\n";
+ v=v+1;
     }
-    int a;
+}
+    else if(opcion == 3){
+     int a;
     a=0;
     while(a<728){
-         cout << "solicito tu ayuda\n";
-         a=a+1;
+        cout << "Informatica se me da muy bien\n";
+a=a+1;
     }
-    int z;
-    z=0;
-    while(z<15000){
-         cout << "informatica se me da muy bien\n";
-         z=z+1;
-    }
+     }
+    else{
+        cout << "Aprende a hacer lo que te dicen\n";
+}
+    return 0;
 }
