b2327e0b992705e5cd7debbda16a6d3919dd0184
carraski <carrasquerrur@ieselaios.com>
2026-02-06 09:52:00 +0000

    valor maximo y minimo en proceso

---
+++ array2_metodo_general.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num[]={1,2,3,4,5};
+    int maximo=num[0]; 
+    for (int i=1;i<5; i++){
+    if (num[i] > maximo){
+    maximo = num[i];
+        }
+        }
+        cout << "el valor máximo es: " << maximo;
+    int minimo=num[0];
+    for (int i=1; i>2; i){
+    if (num[i] <minimo){
+    minimo = num[i];
+    }    
+    }
+        cout << "el valor mínimo es:" << minimo;
+    return 0;
+}
