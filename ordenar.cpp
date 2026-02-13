a01c3ee830808be30d9c0a5b5af1f3701ad59995
carraski <carrasquerrur@ieselaios.com>
2026-02-13 09:51:47 +0000

    hecha la ordenación de números

---
+++ ordenar.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Dime cuantos números quieres ordenar\n";
+    cin >> num;
+    int a[num];
+
+    for(int i=0; i<num; i++){
+    cout <<"Dame el siguiente numero ("<<i<<")";
+    cin >> a[i];
+    }
+
+int menor, posmenor, temp;
+
+    for(int i=0;i<num-1;i++){
+        menor=a[i]; posmenor=i;
+        for(int j=i; j<num; j++){
+            if (a[j]<menor){
+                menor=a[j];posmenor=j;
+            }
+        }
+        temp=a[i];
+        a[i]=a[posmenor];
+        a[posmenor]=temp;
+    }
+
+    int k=0;
+    while (k<num){
+        cout << a[k]<<" ";
+        k++;
+    }
+}
