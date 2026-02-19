30a8e496fad5b9452bed37653146fae4a4086a25
carraski <carrasquerrur@ieselaios.com>
2026-02-19 08:36:46 +0000

    hecho el bubble ya

--- ordenarbubble.cpp
+++ ordenarbubble.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int num;
-    cout << "Dime cuantos números quieres ordenar\n";
-    cin >> num;
-    int a[num];
-    for(int i=0; i<num; i++){
+void Dame(int num, int *a){
+     for(int i=0; i<num; i++){
     cout <<"Dame el siguiente numero ("<<i<<")";
     cin >> a[i];
     }
-
+}
+void Ordename (int num, int *a){
     bool cambio;
     // bool es el tipo de variable que solo puede ser verdadero o falso.
     //aqui lo uso para que pare si no hay cambio en la secuencia o que siga si lo hay.
     int temp;
-    do{
+     do{
         cambio = false;
         for(int i=0; i<num; i++)
             if(a[i]>a[i+1]){
@@ -23,10 +20,25 @@ int main(){
                 a[i+1]=temp;
                 cambio=true;
             } 
-        }while(cambio);
+        
+    }
+}
+void Sacalos (int num, int *a){
+    int cambio;
+   while(cambio);
  int k=0;
     while (k<num){
         cout << a[k]<<" ";
         k++;
     }
+}
+int main (){
+    int num;
+    cout << "dime cuantos numeros me vas a dar";
+    cin >>num;
+    int a[num];
+    Dame(num, &a[0]);
+    // acordarse de que & iba relacionado a la direccion de memoria
+    Ordename (num, &a[0]);
+    Sacalos (num, &a[0]);
 }
