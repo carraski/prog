836f31901e629ed4256bf7e687c2c565582dea9f
carraski <carrasquerrur@ieselaios.com>
2026-03-17 08:59:26 +0000

    un codigo malo y el inicio del bueno

---
+++ IOI-romanos.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "qué número quieres";
+    cin >> n;
+    //se puede usar el switch(n){case 9: cout <<"IX"; case 8: cout <<"XIII";}...etc
+
+
+
+
+}
---
+++ fallitos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int resultado;
+    cout <<"Tenemos que poner el número 39 en numeros romanos\n";
+    cout <<"El máximo valor que podemos dar en una ronda es 10\n";
+    while (resultado!= XXXIX)
+    cout <<"Dime un número";
+    cin >> n;
+        if(n >= 10){
+        cout <<"X";
+        }
+        else{
+            cout <<"IX";
+        }
+    resultado= 
+}
